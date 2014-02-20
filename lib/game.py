dbb0dc3ae3cda8c2f58dff7aa91c7c296035e8bf
AUmrysh <aumrysh@gmail.com>
2014-02-19 20:47:16 -0500

    Updated to work with linux and windows
    added to work with linux, QWOP config

--- lib/game.py
+++ lib/game.py
@@ -1,28 +1,53 @@
-import win32api
-import win32con
+import sys
+
+if sys.platform.startswith('win'):
+    import win32api
+    import win32con
+if sys.platform.startswith('linux'):
+    from predestinate import KeyGod
 
 import time
+from misc import pbutton
 
 class Game:
 
     keymap = {
-        'up': 0x30,
-        'down': 0x31,
-        'left': 0x32,
-        'right': 0x33,
-        'a': 0x34,
-        'b': 0x35,
-        'start': 0x36,
-        'select': 0x37
+        'q':"q",
+        'w':"w",
+        'o':"o",
+        'p':"p",
+        'space':"space"
     }
 
     def is_valid_button(self, button):
         return button in self.keymap.keys()
 
     def button_to_key(self, button):
-        return self.keymap[button]
+        try:
+            return [self.keymap[button], 0.1]
+        except KeyError:
+            #try a command with a timing
+            if len(button) is 2:
+                try:
+                    btnpart = str(button[:1])
+                    numpart = int(button[1:])
+                except ValueError:
+                    return None
+                if button[:1] in self.keymap and int(button[1:]) in xrange(1, 6):
+                    return [self.keymap[btnpart], 0.1*numpart]
 
     def push_button(self, button):
-        win32api.keybd_event(self.button_to_key(button), 0, 0, 0)
-        win32api.keybd_event(self.button_to_key(button), 0, win32con.KEYEVENTF_KEYUP, 0)
-        
+        if sys.platform.startswith('win'):
+            win32api.keybd_event(self.button_to_key(button), 0, 0, 0)
+            win32api.keybd_event(self.button_to_key(button), 0, win32con.KEYEVENTF_KEYUP, 0)
+        if sys.platform.startswith('linux'):
+            btn = self.button_to_key(button)
+            if btn is not None:
+                pbutton(nick, btn)
+                keypress(btn)
+
+def keypress(seq):
+    kg = KeyGod()
+    kg.key_down(seq[0])
+    time.sleep(seq[1])
+    kg.key_up(seq[0])
